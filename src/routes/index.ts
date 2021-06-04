5c41f928dab4df6610afd6ec9eda7bf24cf817d2
leo-jansen <janseleo@gmail.com>
2021-06-04 17:30:27 -0300

    config routes

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const routes = Router();
+
+routes.post("/settings", (req, res) => {
+
+});
+
+export { routes };
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
 
-import "./database"
+import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
+app.use(express.json());
+app.use(routes);
 app.listen(3000, () => console.log("Server rodando!!!"));
