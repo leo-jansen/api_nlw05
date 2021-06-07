74c82dba4811df064d734d1a99359c4085995634
leo-jansen <janseleo@gmail.com>
2021-06-07 04:00:30 -0300

    router post messages

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
+import { MessagesController } from "../controller/MessagesController";
 import { SettigsController } from "../controller/SettingsController";
 import { UsersController } from "../controller/UsersCrontroller";
 
 const routes = Router();
 const settigsController = new SettigsController();
 const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 routes.post("/settings", settigsController.create);
 routes.post("/users", usersController.create)
+routes.post("/messages", messagesController.create)
 
 export { routes };
