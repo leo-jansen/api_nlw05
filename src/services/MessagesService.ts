3627a074baafd01d8b215b64948e842e435b9209
leo-jansen <janseleo@gmail.com>
2021-06-07 03:34:53 -0300

    create service MessagesService

---
+++ src/services/MessagesService.ts
@@ -0,0 +1,21 @@
+import { getCustomRepository } from "typeorm";
+import { MessagesRepository } from "../repositories/MessagesRepository";
+
+interface IMessagesCreate {
+  admin_id: string;
+  user_id: string;
+  text: string;
+}
+
+class MessagesService {
+  async create({ admin_id, user_id, text }: IMessagesCreate) {
+    const messagesRepository = getCustomRepository(MessagesRepository);
+    const message = messagesRepository.create({ admin_id, user_id, text });
+    
+    await messagesRepository.save(message);
+
+    return message;
+  }
+}
+
+export { MessagesService };
