9463b9fe84972e294609d814f3cf7616e04868cc
leo-jansen <janseleo@gmail.com>
2021-06-07 00:04:36 -0300

    router post user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { SettigsController } from "../controller/SettingsController";
+import { UsersController } from "../controller/UsersCrontroller";
 
 const routes = Router();
 const settigsController = new SettigsController();
+const usersController = new UsersController();
 
 routes.post("/settings", settigsController.create);
+routes.post("/users", usersController.create)
 
 export { routes };
