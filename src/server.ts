63d46d1e14ac4070b5dfb0c87d0d51255cd38748
leo-jansen <janseleo@gmail.com>
2021-06-10 01:44:21 -0300

    add websocket

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 
 import "./database";
 import { routes } from "./routes";
 
 const app = express();
+const http = createServer(app);
+const io = new Server(http);
+
 
 app.use(express.json());
 app.use(routes);
