ea3c57a7d847cc50694fd6cc75c1228b7774f890
leo-jansen <janseleo@gmail.com>
2021-06-10 03:37:47 -0300

    add service ConnectionsService

---
+++ src/services/ConnectionsService.ts
@@ -0,0 +1,26 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { Connection } from "../entities/Connection";
+import { ConnectionsRepository } from "../repositories/ConnectionsRepository";
+
+interface IConnectionCreate {
+  id?: string;
+  admin_id?: string;
+  user_id: string;
+  socket_id: string;
+}
+
+class ConnectionsService {
+  private connectionsService: Repository<Connection>;
+
+  constructor() {
+    this.connectionsService = getCustomRepository(ConnectionsRepository);
+  }
+  async create({ socket_id, user_id, admin_id, id }: IConnectionCreate) {
+    const connection = this.connectionsService.create({ socket_id, user_id, admin_id, id });
+    await this.connectionsService.save(connection);
+
+    return connection;
+  }
+}
+
+export { ConnectionsService };
