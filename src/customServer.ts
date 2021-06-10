0e2ddb10387a8f4a822319220b22cd29c491dfe5
leo-jansen <janseleo@gmail.com>
2021-06-10 02:19:34 -0300

    refactor server and add websocket folder

---
+++ src/customServer.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import path from "path";
+
+import "./database";
+import { routes } from "./routes";
+
+const app = express();
+app.use(express.static(path.join(__dirname, "..", "client")));
+app.set("views", path.join(__dirname, "..", "client"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (req, res) => {
+  return res.render("html/client.html");
+})
+
+const http = createServer(app);
+const io = new Server(http);
+
+io.on("connection", (socket: Socket) => {
+  console.log("Conectado", socket.id);
+})
+
+app.use(express.json());
+app.use(routes);
+
+export { io, http };
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,3 @@
-import express from "express";
-import { createServer } from "http";
-import { Server, Socket } from "socket.io";
-import path from "path";
+import { http } from "./customServer";
 
-import "./database";
-import { routes } from "./routes";
-
-const app = express();
-app.use(express.static(path.join(__dirname, "..", "client")));
-app.set("views", path.join(__dirname, "..", "client"));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
-
-app.get("/pages/client", (req, res) => {
-  return res.render("html/client.html");
-})
-
-const http = createServer(app);
-const io = new Server(http);
-
-io.on("connection", (socket: Socket) => {
-  console.log("Conectado", socket.id);
-})
-
-app.use(express.json());
-app.use(routes);
 http.listen(3000, () => console.log("Server rodando!!!"));
